e0096fca2f83ceb1f2c224ab9cf2b06544b2ccdc
Raja-jpeg <59841593+Raja-jpeg@users.noreply.github.com>
2024-07-25 00:52:37 -0400

    Add files via upload

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,1 +1,32 @@
+import random
+import re
 
+wordlist = []
+f = open("wordlist.txt", "r")
+for x in f:
+    wordlist.append(x.replace("\n", ""))
+
+filtered_words = [word for word in wordlist if re.match(r"\b[a-z]{5}\b", word)]
+
+answer = ''.join(random.choices(filtered_words))
+
+print("Guess the 5 letter word")
+chance = 5
+
+while chance > 0:
+    guess = input(f"{chance} chances left: ")
+
+    if len(guess) != 5:
+        print("This is a 5 letter word game, not more, not less.")
+        continue
+    
+    if guess == answer:
+        print("You win")
+        break
+    else:
+        chance -= 1
+        if chance == 0:
+            print("Game Over")
+            print(f"The answer is {answer}")
+        else:
+            print("Wrong guess, try again")
